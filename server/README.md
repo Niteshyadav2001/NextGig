9c67b7f2079c3b092bbdbab0c148948720ce4af5
Nitesh Kumar <nyrao0017@gmail.com>
2025-02-21 23:30:46 +0530

    basic-server-setup-from-starting

--- server/README.md
+++
@@ -1,4 +0,0 @@
-# Sentry :
-- cloud based application
-- ues to monitor and track errors
-- tracking performance
--- server/server.js
+++ server/server.js
@@ -1,34 +1,26 @@
-import './config/instrument.js';
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './config/db.js';
-import * as Sentry from "@sentry/node";
-import {clerkWebhooks} from './controllers/webhooks.js'
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
 
 
-//Initialise EXPRESS
+
+// initiaze express
 const app = express()
 
-//Connect to Databse
-await connectDB()
+
+// middlewares
+app.use(cors())
+app.use(express.json())
 
 
-//Middlewares
-app.use(cors());
-app.use(express.json());
+// Routes
+app.get('/',(req,res) => res.send("API working"))
 
-//Routes
-app.get('/',(req,res) => res.send("API Working"));
-app.get("/debug-sentry", function maninHandler(req,res){
-    throw new Error("my first sentry error!");
-});
-app.post('/webhook', clerkWebhooks)
 
-//Port
-const PORT = process.env.PORT || 5000;
-Sentry.setupExpressErrorHandler(app);
+// port
+const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on ${PORT}`)
+})
